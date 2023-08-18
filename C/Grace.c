1d18e0b005ea2d72752b376ebeac00303f524a75
alisterd51 <antoinereims28@gmail.com>
2023-08-19 00:09:21 +0200

    update Grace

--- C/Grace.c
+++ C/Grace.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-#include <fcntl.h>
 
-#define GRACE "#include <stdio.h>%2$c#include <fcntl.h>%2$c%2$c#define GRACE %3$c%1$s%3$c%2$c%2$cint main() {%2$c    int fd = open(%3$cGrace_kid.c%3$c, O_WRONLY | O_CREAT, 0664);%2$c%2$c    dprintf(fd, GRACE, GRACE, %5$c%4$cn%5$c, %5$c%4$c%3$c%5$c, %5$c%4$c%4$c%5$c, %5$c%4$c%5$c%5$c);%2$c}%2$c"
+#define GRACE "#include <stdio.h>%2$c%2$c#define GRACE %3$c%1$s%3$c%2$c#define MAIN() int main() {FILE *stream = fopen(%3$cGrace_kid.c%3$c, %3$cw+%3$c); fprintf(stream, GRACE, GRACE, %5$c%4$cn%5$c, %5$c%4$c%3$c%5$c, %5$c%4$c%4$c%5$c, %5$c%4$c%5$c%5$c); return (0);}%2$c%2$cMAIN()%2$c"
+#define MAIN() int main() {FILE *stream = fopen("Grace_kid.c", "w+"); fprintf(stream, GRACE, GRACE, '\n', '\"', '\\', '\''); return (0);}
 
-int main() {
-    int fd = open("Grace_kid.c", O_WRONLY | O_CREAT, 0664);
-
-    dprintf(fd, GRACE, GRACE, '\n', '\"', '\\', '\'');
-}
+MAIN()
