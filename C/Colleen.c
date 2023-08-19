9a88e2fe73ce1f3f93e3145b0f3ee29f5cf89a60
alisterd51 <antoinereims28@gmail.com>
2023-08-19 03:29:13 +0200

    fix Colleen compil

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define COLLEEN "#include <stdio.h>%2$c%2$c#define COLLEEN %3$c%1$s%3$c%2$c%2$c/*%2$c *comment two%2$c */%2$c%2$cvoid test() {%2$c    printf(COLLEEN, COLLEEN, %5$c%4$cn%5$c, %5$c%4$c%3$c%5$c, %5$c%4$c%4$c%5$c, %5$c%4$c%5$c%5$c);%2$c}%2$c%2$cint main() {%2$c    /*comment one*/%2$c    test();%2$c}%2$c"
+#define COLLEEN "#include <stdio.h>%2$c%2$c#define COLLEEN %3$c%1$s%3$c%2$c%2$c/*%2$c *comment two%2$c */%2$c%2$cvoid test() {%2$c    printf(COLLEEN, COLLEEN, %5$c%4$cn%5$c, %5$c%4$c%3$c%5$c, %5$c%4$c%4$c%5$c, %5$c%4$c%5$c%5$c);%2$c}%2$c%2$cint main() {%2$c    /*comment one*/%2$c    test();%2$c    return (0);%2$c}%2$c"
 
 /*
  *comment two
@@ -13,4 +13,5 @@ void test() {
 int main() {
     /*comment one*/
     test();
+    return (0);
 }
