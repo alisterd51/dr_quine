7896b4b986040054f82907d5ae8c3d2a1d4d0d5c
Antoine Clarman <antoinereims28@gmail.com>
2024-09-19 17:34:29 +0200

    feat(asm): add grace

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,11 +1,14 @@
 extern printf
 
 section .data
-    msg: db "extern printf%1$c%1$csection .data%1$c    msg: db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$c    global main%1$cmain:%1$c    push rbp%1$c    mov rdi, msg%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    mov rcx, msg%1$c    mov rax, 0%1$c    call printf wrt ..plt%1$c    pop rbp%1$c    mov rax, 0%1$c    ret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c", 0
+    msg: db "extern printf%1$c%1$csection .data%1$c    msg: db %2$c%3$s%2$c, 0%1$c%1$c; comment two%1$c%1$csection .text%1$c    global main%1$cmain:%1$c    ; comment one%1$c    push rbp%1$c    mov rdi, msg%1$c    mov rsi, 10%1$c    mov rdx, 34%1$c    mov rcx, msg%1$c    mov rax, 0%1$c    call printf wrt ..plt%1$c    pop rbp%1$c    mov rax, 0%1$c    ret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c", 0
+
+; comment two
 
 section .text
     global main
 main:
+    ; comment one
     push rbp
     mov rdi, msg
     mov rsi, 10
--- C/Grace.c
+++ C/Grace.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-#define GRACE "#include <stdio.h>%2$c%2$c#define GRACE %3$c%1$s%3$c%2$c#define MAIN() int main() {FILE *stream = fopen(%3$cGrace_kid.c%3$c, %3$cw+%3$c); fprintf(stream, GRACE, GRACE, %5$c%4$cn%5$c, %5$c%4$c%3$c%5$c, %5$c%4$c%4$c%5$c, %5$c%4$c%5$c%5$c); return (0);}%2$c%2$c/* comment */%2$c%2$cMAIN()%2$c"
-#define MAIN() int main() {FILE *stream = fopen("Grace_kid.c", "w+"); fprintf(stream, GRACE, GRACE, '\n', '\"', '\\', '\''); return (0);}
+#define GRACE "#include <stdio.h>%2$c%2$c#define GRACE %3$c%1$s%3$c%2$c#define MAIN() int main() {FILE *stream = fopen(%3$cGrace_kid.c%3$c, %3$cw+%3$c); fprintf(stream, GRACE, GRACE, %5$c%4$cn%5$c, %5$c%4$c%3$c%5$c, %5$c%4$c%4$c%5$c, %5$c%4$c%5$c%5$c); fclose(stream); return (0);}%2$c%2$c/* comment */%2$c%2$cMAIN()%2$c"
+#define MAIN() int main() {FILE *stream = fopen("Grace_kid.c", "w+"); fprintf(stream, GRACE, GRACE, '\n', '\"', '\\', '\''); fclose(stream); return (0);}
 
 /* comment */
 
